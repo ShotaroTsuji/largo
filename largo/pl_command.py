899980d4ab0345c7f57a1b92da8d8001331f247a
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2022-01-02 23:59:40 +0900

    Add handler of pl subcommand date argument

--- largo/pl_command.py
+++ largo/pl_command.py
@@ -26,13 +26,27 @@ class PlCommand(Command):
 
     pl
         {--manifest-path=Largo.toml : The path to a manifest file}
-        {month? : Month to be shown}
+        {date-argument? : Year/month to be shown}
     """
 
     def handle(self):
         project = Project(manifest_path=self.option('manifest-path'))
-        year = project.latest_year()
-        date_range = DateRange(year, self.argument('month'))
+
+        date_argument = self.argument('date-argument')
+
+        if date_argument:
+            arg_type, arg = parse_argument(date_argument)
+            if arg_type is ArgumentType.YEAR:
+                year = arg
+                month = None
+            elif arg_type is ArgumentType.MONTH:
+                year = project.latest_year()
+                month = arg
+        else:
+            year = project.latest_year()
+            month = None
+
+        date_range = DateRange(year, month)
 
         pl = ProfitLoss(project, date_range=date_range)
         pl.build(year)
