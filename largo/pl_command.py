d08fadb2cbb9f11f5edd6b5b0781544f8ce899f7
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2022-01-03 00:01:53 +0900

    Add `largo pl <year>` which shows the profit/loss of the given year (#27)
    * Add `largo pl <year>` which shows the profit/loss of the given year
    
    - [ ] Parse pl subcommand argument
    
    * Add parsing pl subcommand argument
    
    It parses type following sorts of arguments:
      - year like `2020`.
      - month like `dec`.
    
    * Add handler of pl subcommand date argument
    
    * Supress pyright error for unbound variables

--- largo/pl_command.py
+++ largo/pl_command.py
@@ -2,6 +2,22 @@ from cleo import Command
 from largo.date_range import DateRange
 from largo.project import Project
 from largo.profit_loss import ProfitLoss
+from typing import Any, Tuple
+import enum
+import re
+
+
+class ArgumentType(enum.Enum):
+    YEAR = enum.auto()
+    MONTH = enum.auto()
+
+
+def parse_argument(s: str) -> Tuple[ArgumentType, Any]:
+    if re.match(r"\d+", s):
+        return (ArgumentType.YEAR, int(s))
+    if re.match(r"[a-z]+", s):
+        return (ArgumentType.MONTH, s)
+    raise Exception(f'unsupported type of argument {s}')
 
 
 class PlCommand(Command):
@@ -10,13 +26,29 @@ class PlCommand(Command):
 
     pl
         {--manifest-path=Largo.toml : The path to a manifest file}
-        {month? : Month to be shown}
+        {date-argument? : Year/month to be shown}
     """
 
     def handle(self):
         project = Project(manifest_path=self.option('manifest-path'))
+
+        date_argument = self.argument('date-argument')
+
         year = project.latest_year()
-        date_range = DateRange(year, self.argument('month'))
+        month = None
+        if date_argument:
+            arg_type, arg = parse_argument(date_argument)
+            if arg_type is ArgumentType.YEAR:
+                year = arg
+                month = None
+            elif arg_type is ArgumentType.MONTH:
+                year = project.latest_year()
+                month = arg
+        else:
+            year = project.latest_year()
+            month = None
+
+        date_range = DateRange(year, month)
 
         pl = ProfitLoss(project, date_range=date_range)
         pl.build(year)
