50e7734c04d7a8001d9ea55a2e7198a719182946
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2022-01-03 00:34:19 +0900

    Add `largo pl <year-month>` which shows the P/L of the given month (#28)
    * Add `largo pl <year-month>` which shows the P/L of the given month
    
    - [ ] Modify the pl subcommand argument parser
    
    * Add year/month argument parser
    
    * Add year/month argument handler

--- largo/date_range.py
+++ largo/date_range.py
@@ -1,9 +1,14 @@
 import datetime
 
+def month_abbrevs() -> list[str]:
+    return ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
+            'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+
+def month_to_abbrev(i: int) -> str:
+    return month_abbrevs()[i - 1]
+
 def month_from_abbrev(s: str) -> int:
-    abbrevs = ['jan', 'feb', 'mar', 'apr', 'may', 'jun',
-               'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-    return abbrevs.index(s) + 1
+    return month_abbrevs().index(s) + 1
 
 class DateRange:
     def __init__(self, year, month=None) -> None:
