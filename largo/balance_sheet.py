ead9fe38dcdec4dafa1e7a76e46d935e0c408271
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2022-01-08 20:07:51 +0900

    Use `DateRange` object instead of `year: int` in `BalanceSheet`

--- largo/balance_sheet.py
+++ largo/balance_sheet.py
@@ -1,28 +1,36 @@
-import subprocess
 from largo.project import Project
 from largo.ledger_invoke import LedgerInvoke
+from largo.date_range import DateRange
 from typing import List
 
 
 class BalanceSheet(LedgerInvoke):
-    def __init__(self, project: Project, year: int):
+    def __init__(self, project: Project, date_range: DateRange):
         self._project = project
-        self._year = year
+        self._date_range = date_range
 
     @property
     def project(self) -> Project:
         return self._project
 
     @property
+    def accounts(self) -> list[str]:
+        return [self.project.account.assets,
+                self.project.account.liabilities,
+                self.project.account.equity]
+
+    @property
     def year(self) -> int:
-        return self._year
+        return self._date_range.year
 
     @property
     def command_arguments(self) -> List[str]:
-        arguments = [self.project.ledger_bin, '-f', '-', 'balance',
-                     self.project.account.assets,
-                     self.project.account.liabilities,
-                     self.project.account.equity]
+        arguments = [self.project.ledger_bin, '-f', '-', 'balance']
+
+        if self._date_range:
+            arguments.extend(['-b', str(self._date_range.begin), '-e', str(self._date_range.end)])
+
+        arguments.extend(self.accounts)
 
         settings = self.project.bs_command
         if settings:
--- largo/bs_command.py
+++ largo/bs_command.py
@@ -1,7 +1,7 @@
 from cleo import Command
 from largo.project import Project
 from largo.balance_sheet import BalanceSheet
-import datetime
+from largo.date_range import DateRange, date_argument_to_date_range
 
 
 class BsCommand(Command):
@@ -9,18 +9,16 @@ class BsCommand(Command):
     Show balance sheet
 
     bs
-        {year? : The year of the balance sheet}
+        {date-argument? : The year/month of the balance sheet}
         {--manifest-path=Largo.toml : The path to a manifest file}
     """
 
     def handle(self):
         project = Project(manifest_path=self.option('manifest-path'))
 
-        year = self.argument('year')
-        if year:
-            year = int(year)
-        else:
-            year = project.latest_year()
+        date_argument = self.argument('date-argument')
 
-        bs = BalanceSheet(project, year)
+        date_range = date_argument_to_date_range(date_argument, default_year=project.latest_year())
+
+        bs = BalanceSheet(project, date_range)
         bs.build()
