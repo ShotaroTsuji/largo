419d243157938ae8a61146fa6c4e4ccac9f8256f
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2022-01-08 20:54:06 +0900

    Modify `largo bs` to take period arguments like `largo pl`. (#32)
    * Modify `largo bs` to take period arguments like `largo pl`.
    
    - [ ] Merge the develop branch after PR #31 is merged.
    - [ ] Modify to pass `DateRange` object to the `build` method.
    
    * Use `DateRange` object instead of `year: int` in `BalanceSheet`
    
    * Refactor command argument building function
    
    It is refactored out into the abstract class `LedgerInvoke`.
    Now concrete classes only supply parameters.

--- largo/balance_sheet.py
+++ largo/balance_sheet.py
@@ -1,31 +1,44 @@
-import subprocess
 from largo.project import Project
 from largo.ledger_invoke import LedgerInvoke
+from largo.date_range import DateRange
 from typing import List
+import datetime
 
 
 class BalanceSheet(LedgerInvoke):
-    def __init__(self, project: Project, year: int):
+    def __init__(self, project: Project, date_range: DateRange):
         self._project = project
-        self._year = year
+        self._date_range = date_range
 
     @property
     def project(self) -> Project:
         return self._project
 
     @property
-    def year(self) -> int:
-        return self._year
+    def accounts(self) -> List[str]:
+        return [self.project.account.assets,
+                self.project.account.liabilities,
+                self.project.account.equity]
 
     @property
-    def command_arguments(self) -> List[str]:
-        arguments = [self.project.ledger_bin, '-f', '-', 'balance',
-                     self.project.account.assets,
-                     self.project.account.liabilities,
-                     self.project.account.equity]
+    def year(self) -> int:
+        return self._date_range.year
 
-        settings = self.project.bs_command
-        if settings:
-            arguments.extend(settings.default_options)
+    @property
+    def ledger_subcommand(self) -> str:
+        return 'balance'
 
-        return arguments
+    @property
+    def period_begin(self) -> datetime.date:
+        return self._date_range.begin
+
+    @property
+    def period_end(self) -> datetime.date:
+        return self._date_range.end
+
+    @property
+    def default_options(self) -> List[str]:
+        if settings := self.project.bs_command:
+            return settings.default_options
+        else:
+            return []
